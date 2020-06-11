a25bc738a61cef26d49a30bf2c691139ec99aee4
jsissler <jsissler@gmail.com>
2020-06-11 14:19:46 -0400

    no need for window

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -33,7 +33,7 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-(function(window) {
+(function() {
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
   // STEP 10:
@@ -62,4 +62,4 @@ WARNING!!! WARNING!!!
       helloSpeaker.speak(names[i]);
     }
   }
-})(window);
+})();
